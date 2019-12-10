c63d7a2f0e1ff654033c701ea1d4396713f0890f
irene macapinlac <irenekaymac@gmail.com>
2019-12-10 15:17:31 +1100

    css almost done, started on javaScript

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    // console.log( "ready!" );
+
+//when player 1's name is entered print on the left $('.player1') and choose symbol on dropdown.
+
+$('#nameform').on('click', function(){
+
+};
+
+//If a player connects three symbols next to eachother then the player wins.
+//If both players fill the board without connecting three symbols then it's a draw.
+
+// if
+
+
+
+
+
+
+
+
+
+
+});
